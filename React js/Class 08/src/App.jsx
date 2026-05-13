de4db21e0a638d33387f387d10606308ffdfd745
Muhammad-Faizan3 <fb240951@gmail.com>
2026-05-13 17:54:23 +0500

    Two way Data Binding

---
+++ React js/Class 08/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import DataBinding from './Components/DataBinding'
+// import Parent from './Components/Parent'
+
+function App() {
+
+  return (
+    <>
+    {/* <Parent />   */}
+    <DataBinding />    
+    </>
+  )
+}
+
+export default App
---
+++ React js/Class 08/src/Components/DataBinding.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function DataBinding() {
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+
+    console.log(name);
+    console.log(email);
+    console.log('rander');
+
+    let obj = {
+        name1: name,
+        email1: email
+    }
+    console.log(obj);
+    
+    
+    return(
+        <div>
+            <h3>Two way Data Binding</h3>
+            <p>Name: {name}</p>
+            <p>Email: {email}</p>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" value={email}  onChange={(e) => setEmail(e.target.value)}/>
+            <button>Submit</button>
+        </div>
+    )
+}
+export default DataBinding;
